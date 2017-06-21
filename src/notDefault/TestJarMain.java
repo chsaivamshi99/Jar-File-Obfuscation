d94cde542627b9978b18e32aaa438945677ca27a
chsaivamshi99 <chsaivamshi99@gmail.com>
2017-06-21 11:24:43 +0530

    Initial Commit

---
+++ src/notDefault/TestJarMain.java
@@ -0,0 +1,22 @@
+package notDefault;
+//use this to create jar file
+//uses eclipse export option to create jar file
+//remember to include manifest file while exporting
+//https://www.cs.utexas.edu/~scottm/cs307/handouts/Eclipse%20Help/jarInEclipse.htm
+
+//execute below from folder containing jar to execute it
+//java -jar Tester.jar
+
+//follow this to obfuscate jar file
+//How to obfuscate a jar File with Proguard
+//https://www.youtube.com/watch?v=MeQosb-pP6A
+
+//download and run decompiler from this
+//http://jd.benow.ca/
+//use gui to decompile jars
+
+public class TestJarMain {
+	public static void main(String[] args) {
+		System.out.println("Njoy");
+	}
+}
